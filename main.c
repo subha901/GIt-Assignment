e76405df55e7102e631998127af35742c7773c61
Subhabrata Das <111212033+subha901@users.noreply.github.com>
2023-05-17 21:24:18 +0530

    Update main.c
    Demo prctice

--- main.c
+++ main.c
@@ -1,15 +1,2 @@
-pseudo code
-{
-Initial Function()
-{
-  //Sample Code + feature1
-}
 
-Initial Security() + security patch(latest)
-{
- //Sample Code + feature2
-}
 This is just adding line in this repo , for testing jenkins
-
-
-}
